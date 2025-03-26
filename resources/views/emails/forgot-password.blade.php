9d804cdea66857d5a7d75fffd4fc1cb7fdc3cd30
jeet <jeetrathodinitiontechmedia@gmail.com>
2025-03-26 17:40:14 +0530

    change

--- resources/views/emails/forgot-password.blade.php
+++ resources/views/emails/forgot-password.blade.php
@@ -13,5 +13,7 @@
     <p>Your OTP for password reset is: <strong>{{ $otp }}</strong></p>
     <p>This OTP is valid for 10 minutes.</p>
     <p>If you did not request a password reset, please ignore this email.</p>
+    <p>If you did not request a password reset, please ignore this email.</p>
+
 </body>
 </html>
