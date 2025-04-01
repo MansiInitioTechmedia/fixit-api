12ba3cbc0762c231eb431ed6a89e9a43ee30fb2a
jeet <jeetrathodinitiontechmedia@gmail.com>
2025-04-01 15:57:52 +0530

    Service date changed

--- database/migrations/2025_03_27_121204_create_schedules_table.php
+++ database/migrations/2025_03_27_121204_create_schedules_table.php
@@ -17,7 +17,7 @@ return new class extends Migration {
             $table->date('expiration_date')->nullable()->default(null);
             $table->integer('kilometers')->nullable()->default(null);
             $table->integer('status'); // Store status as integer (0, 1, 2, 3)
-            $table->date('service_date')->default(DB::raw('CURRENT_DATE')); // New column for storing the current date
+            $table->date('service_date'); // New column for storing the current date
             $table->timestamps();
         });
 
